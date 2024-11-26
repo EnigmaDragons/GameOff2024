745b5f3491483d03c4beb84fd2125ed2c8d6bd7c
Charles Cooper <cmcooper@uchicago.edu>
2024-11-25 21:38:36 -0600

    NPCs walk again

--- src/GameOff2024/Assets/Scripts/Character/Npc/SkinnedNpc.cs
+++ src/GameOff2024/Assets/Scripts/Character/Npc/SkinnedNpc.cs
@@ -2,54 +2,20 @@ using UnityEngine;
 
 public class SkinnedNpc : MonoBehaviour
 {
-    [SerializeField] private Animator[] animators;
     [SerializeField] private SkinnedMeshRenderer[] renderers;
     [SerializeField] private float animationDistance = 36f;
 
-    private int _selectedIndex;
-    private Animator activeAnimator;
+    [SerializeField] private Animator activeAnimator;
     private SkinnedMeshRenderer activeRenderer;
     private bool isInRange;
 
     public SkinnedMeshRenderer Renderer => activeRenderer; 
-    
-    private void Awake()
+
+    public void SetShouldAnimate(bool shouldAnimate)
     {
-        if (animators == null || animators.Length == 0)
-        {
-            Debug.LogError("No animators assigned to SkinnedNpc");
-            return;
-        }
-
-        // Pick a random animator
-        int randomIndex = Random.Range(0, animators.Length);
-        _selectedIndex = randomIndex;
-        activeAnimator = animators[randomIndex];
-
-        // Set active state for all animator GameObjects in one pass
-        for (int i = 0; i < animators.Length; i++)
-        {
-            if (animators[i] != null)
-            {
-                animators[i].gameObject.SetActive(i == randomIndex);
-            }
-        }
+        isInRange = shouldAnimate;
+        activeAnimator.enabled = shouldAnimate;
     }
-
-    private void Update()
-    {
-        if (activeAnimator == null || CurrentGameState.ReadOnly.playerTransform == null) return;
-
-        float distanceToPlayer = Vector3.Distance(transform.position, CurrentGameState.ReadOnly.playerTransform.position);
-        bool shouldBeInRange = distanceToPlayer <= animationDistance;
-
-        if (shouldBeInRange != isInRange)
-        {
-            isInRange = shouldBeInRange;
-            activeAnimator.enabled = isInRange;
-        }
-    }
-
     public void SetTrigger(string triggerName)
     {
         if (activeAnimator != null && isInRange)
